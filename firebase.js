89422a8b8d9e513508613ba8c7747bc90fd3380a
Hicham Baydoun <hicham.baydoun193@gmail.com>
2024-09-04 20:05:18 +0300

    fixed

--- firebase.js
+++ firebase.js
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  NEXT_PUBLIC_EMAILJS_USER_ID=7NUhYA1XjBRX6qpii
+  NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyAXLC6w01tjsjM9HZyzbxb3TR7iqQGCd0U
+  NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=p6-final-a7c1e.firebaseapp.com
+  NEXT_PUBLIC_FIREBASE_PROJECT_ID=p6-final-a7c1e
+  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=p6-final-a7c1e.appspot.com
+  NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=761110869381
+  NEXT_PUBLIC_FIREBASE_APP_ID=1:761110869381:web:dc78fafccff988e3e8c0ec
+  NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID="G-WF5G6P20H2"
 };
 
 // Initialize Firebase
